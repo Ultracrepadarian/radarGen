45f3e11c52e4a77eba9eb83688631ba3f16085a4
Guillermo Martinez <gmartinez@appddictionstudio.com>
2022-08-08 21:42:04 -0500

    Creating Class Shape

--- object.py
+++ object.py
@@ -15,3 +15,19 @@ class paper:
         paper.tk.canvas = tkinter.Canvas(paper.tk)
         paper.tk.canvas.pack(fill=BOTH, expand=1)
 
+class Shape() :
+
+    def __init__(self, width=50, height=50, x=None, y=None, color=black):
+        """
+        Creates a generic 'shape' which contains properties common to all
+        shapes such as height, width, x y coordinates and colour.
+
+        Args:
+            width (int): The width of the shape. Defaults to 50.
+            height (int): The height of the shape. Defaults to 50.
+            x (int): The x position of the shape. If None, the x position will be the middle of the screen. Defaults to None.
+            y (int): The y position of the shape. If None, the y position will be the middle of the screen. Defaults to None.
+            color (string): The color of the shape. Defaults to "black"
+        """
+        if Paper.tk is None:
+            raise Exception("A Paper object has not been created. There is nothing to draw on.")
